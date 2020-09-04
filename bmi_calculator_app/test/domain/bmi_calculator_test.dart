c2af6cf09c533b62ee03efe1064c210785a542d6
Jukka Nikki <jukka.nikki@jambit.com>
2020-09-04 09:30:42 +0200

    Update bmi_calculator_test.dart

--- bmi_calculator_app/test/domain/bmi_calculator_test.dart
+++ bmi_calculator_app/test/domain/bmi_calculator_test.dart
@@ -12,7 +12,20 @@ void main() {
       expect(bmi, 24.22);
     });
 
-    // TODO: add null variants of test here
+    test('bmi should not be calculated and rounded as weight is null', () {
+      double bmi = BmiCalculator.calculateBmiPrecision2(170, null);
+      expect(bmi, null);
+    });
+
+    test('bmi should not be calculated and rounded as height is null', () {
+      double bmi = BmiCalculator.calculateBmiPrecision2(null, 70);
+      expect(bmi, null);
+    });
+
+    test('bmi should not be calculated and rounded as params are null', () {
+      double bmi = BmiCalculator.calculateBmiPrecision2(null, null);
+      expect(bmi, null);
+    });
 
   });
 
