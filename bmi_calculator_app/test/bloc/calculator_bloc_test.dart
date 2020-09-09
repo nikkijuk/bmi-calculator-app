29d1def03f8ad434b8788b07978a6ef15a0e2fd9
Jukka Nikki <jukka.nikki@jambit.com>
2020-09-09 12:00:57 +0200

    Update calculator_bloc_test.dart

--- bmi_calculator_app/test/bloc/calculator_bloc_test.dart
+++ bmi_calculator_app/test/bloc/calculator_bloc_test.dart
@@ -1,6 +1,7 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:bmi_calculator_app/bloc/calculator_bloc.dart';
 import 'package:bmi_calculator_app/domain/traditional_bmi_calculator.dart';
+import 'package:bmi_calculator_app/domain/trefethen_bmi_calculator.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 // test calculator bloc initial state and sending events using sink / receiving state as responses from stream
@@ -15,7 +16,7 @@ void main() {
       expect(CalculatorBloc(TraditionalBmiCalculator()).state.height, null);
     });
 
-    test('initial state of qeight is null', () {
+    test('initial state of weight is null', () {
       expect(CalculatorBloc(TraditionalBmiCalculator()).state.weight, null);
     });
 
@@ -23,21 +24,33 @@ void main() {
       expect(CalculatorBloc(TraditionalBmiCalculator()).state.bmi, null);
     });
 
-    group('calculate', () {
-      blocTest<CalculatorBloc, CalculatorState>(
-        'to events created in response to set height und weight',
-        build: () => CalculatorBloc(TraditionalBmiCalculator()),
-        act: (bloc) => bloc
-          ..add(CalculatorHeightChanged(height: 170))
-          ..add(CalculatorWeightChanged(weight: 70)),
-        expect: const <CalculatorState>[
-          CalculatorState(height: 170, weight: null, bmi: null),
-          CalculatorState(height: 170, weight: 70, bmi: 24.22)
-        ],
-      );
+  });
 
-      // TODO: add test for reset ?
+  group('calculation of bni with traditional algorithm', () {
+    blocTest<CalculatorBloc, CalculatorState>(
+      'to events created in response to set height und weight',
+      build: () => CalculatorBloc(TraditionalBmiCalculator()),
+      act: (bloc) => bloc
+        ..add(CalculatorHeightChanged(height: 170))
+        ..add(CalculatorWeightChanged(weight: 70)),
+      expect: const <CalculatorState>[
+        CalculatorState(height: 170, weight: null, bmi: null),
+        CalculatorState(height: 170, weight: 70, bmi: 24.22)
+      ],
+    );
+  });
 
-    });
+  group('calculation of bni with trefethen algorithm', () {
+    blocTest<CalculatorBloc, CalculatorState>(
+      'to events created in response to set height und weight',
+      build: () => CalculatorBloc(TrefethenBmiCalculator()),
+      act: (bloc) => bloc
+        ..add(CalculatorHeightChanged(height: 170))
+        ..add(CalculatorWeightChanged(weight: 70)),
+      expect: const <CalculatorState>[
+        CalculatorState(height: 170, weight: null, bmi: null),
+        CalculatorState(height: 170, weight: 70, bmi: 24.15)
+      ],
+    );
   });
 }
