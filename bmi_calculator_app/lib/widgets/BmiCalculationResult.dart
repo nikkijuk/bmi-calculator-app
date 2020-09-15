829d8cc520137c88f0cae42af43a6da90a65e100
Jukka Nikki <jukka.nikki@jambit.com>
2020-09-15 09:45:33 +0200

    added some color & text description of bmi result

--- bmi_calculator_app/lib/widgets/BmiCalculationResult.dart
+++ bmi_calculator_app/lib/widgets/BmiCalculationResult.dart
@@ -6,20 +6,49 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 // Complexity of this single component and it's dependencies are reason
 // why it has been moved to separate file - there's no need to reuse it
 class BmiCalculationResult extends StatelessWidget {
+
+  /// For data see:
+  /// - https://www.euro.who.int/en/health-topics/disease-prevention/nutrition/a-healthy-lifestyle/body-mass-index-bmi
+  // - https://www.calculator.net/bmi-calculator.html
+  /*
+    Severe Thinness	< 16
+    Moderate Thinness	16 - 17
+    Mild Thinness	17 - 18.5
+    Normal	18.5 - 25
+    Overweight	25 - 30
+    Obese Class I	30 - 35
+    Obese Class II	35 - 40
+    Obese Class III	> 40
+    */
+
+  // it might be wise to put this data later on to own file - now it's here locally since we only need it here
+  // NOTE: Colors are from dart.ui package
+  final statuses = [
+    NutritionalStatus ('T3', 'Severe Thinness', 0, 16, Colors.blue),
+    NutritionalStatus ('T2', 'Moderate Thinness', 16, 17, Colors.blue.shade100),
+    NutritionalStatus ('T1', 'Mild Thinness', 17, 18.5, Colors.blue.shade200),
+    NutritionalStatus ('N',  'Normal weight', 18.5, 25, Colors.green),
+    NutritionalStatus ('PO', 'Overweight', 25.0, 30, Colors.orange),
+    NutritionalStatus ('O1', 'Obese class I', 30.0, 35, Colors.red.shade200),
+    NutritionalStatus ('O2', 'Obese class II', 35.0, 40, Colors.red.shade100),
+    NutritionalStatus ('O3', 'Obese class III', 40.0, 100, Colors.red),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<CalculatorBloc, CalculatorState>(
       buildWhen: (previous, current) => previous.bmi != current.bmi,
       builder: (context, state) {
-        return state.bmi == null ?
-        Center(
+        if (state.bmi == null) {
+          return Center(
           child: Text(
             "not calculated",
             key: ValueKey ("not-calculated"),
           ),
-        )
-            :
-        Container(
+        );
+        } else {
+          NutritionalStatus current = statuses.lastWhere((element) => element.minBmi <= state.bmi);
+          return Container(
           height: MediaQuery.of(context).size.height * 30/100,
           width:MediaQuery.of(context).size.width * 60/100,
           child: Column(
@@ -27,7 +56,12 @@ class BmiCalculationResult extends StatelessWidget {
               Text(
                 "Bmi is ${state.bmi}",
                 key: ValueKey ("bmi"),
-                style: Theme.of(context).textTheme.headline4,
+                style: Theme.of(context).textTheme.headline4.apply(color: current.color),
+              ),
+              Text(
+                current.name,
+                key: ValueKey ("bmiName"),
+                style: Theme.of(context).textTheme.headline6.apply(color: current.color),
               ),
               Expanded(
                 child: charts.BarChart (
@@ -46,24 +80,15 @@ class BmiCalculationResult extends StatelessWidget {
             ],
           ),
         );
+        }
       },
     );
   }
 
 
   /// Create series list with multiple series
-  /// For data see: https://www.euro.who.int/en/health-topics/disease-prevention/nutrition/a-healthy-lifestyle/body-mass-index-bmi
   List<charts.Series<NutritionalStatus, String>> _nutritionalStatuses(double bmi) {
 
-    // it might be wise to put this data later on to own file - now it's here locally since we only need it here
-    final statuses = [
-      NutritionalStatus ('U', 'Underweight', 0, 18.5, Colors.red.shade50),
-      NutritionalStatus ('N', 'Normal weight', 18.5, 25, Colors.green),
-      NutritionalStatus ('PO', 'Pre-obesity', 25.0, 30, Colors.red.shade50),
-      NutritionalStatus ('O1', 'Obesity class I', 30.0, 35, Colors.red.shade100),
-      NutritionalStatus ('O2', 'Obesity class II', 35.0, 40, Colors.red.shade200),
-      NutritionalStatus ('O3', 'Obesity class III', 40.0, 100, Colors.red.shade400),
-    ];
 
     final result = [
       NutritionalStatus ('ME', 'My BMI', bmi, bmi, Colors.black),
@@ -74,6 +99,7 @@ class BmiCalculationResult extends StatelessWidget {
         id: 'BMI Scale',
         domainFn: (NutritionalStatus status, _) => "BMI",
         measureFn: (NutritionalStatus status, _) => status.maxBmi - status.minBmi,
+        // NOTE: Colors which charts uses are from charts_flutter package - one needs to have manual conversion fo dart.ui colors
         colorFn: (NutritionalStatus status, _) => charts.Color(r: status.color.red, g: status.color.green, b: status.color.blue),
         data: statuses,
       ),
